311a7879f421f5fd0cd7d487a791ea2885fb82cb
Bubu <b11117029@gemail.yuntech.edu.tw>
2024-05-31 16:49:35 +0800

    card.jsx

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,11 @@
+/**
+ * @author B11117061 DickLau <B11117061@gemail.yuntech.edu.tw>
+ */
+
+export default function CustomCard({item}) {
+    return(
+        <div class="border">
+            <div>{item.ScenicSpotName}</div>
+        </div>
+    )
+}
